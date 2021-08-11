770530ec0b36c68195d60dad40b9f23ca789ddac
Victor_J_L <victorjolou@gmail.com>
2021-08-11 16:40:17 -0300

    Feito novos urls paths e conexões com o app e o main

--- web_test/web_test/urls.py
+++ web_test/web_test/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from django.urls.conf import include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('web_test_app.urls')),
 ]
